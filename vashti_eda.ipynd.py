c8eadae186f6d16ad624cdc278eb0f75fa876e14
Vashti259 <156109212+Vashti259@users.noreply.github.com>
2024-02-14 12:49:31 -1100

    vashti_eda.ipynd.py

--- vashti_eda.ipynd.py
+++ vashti_eda.ipynd.py
@@ -1,7 +1,12 @@
 Name: Vashti Gambol
 Project:  06 datafun eda with Jupyter Notebook
 February 14th 2024
+# Overview
 A Data Analyst will be able to use some very important tools such as GitHub, Jupyter, Pandas Seaborn, Git,
-and more.
-With VSCODE installed on their machine, they can do great things.
+and more. With VSCODE installed on their machine, they can do great things.
 
+## Import dependencies
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy
